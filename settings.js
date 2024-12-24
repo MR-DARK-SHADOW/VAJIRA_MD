7db798d3ae42f4b8815188ad4e76d76826d29525
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2024-12-24 13:52:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=zY8zDKjJ#QUPT59PCYy2n6Z32NhzZF08vi6Fxsgk6pbjzmyOSiZQ' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
